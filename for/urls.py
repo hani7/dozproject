af89d5de3982e50776b883aff900c26078b4973b
hani7 <h.informatique7@gmail.com>
2026-05-17 21:33:28 +0100

    first commit

--- for/urls.py
+++ for/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('declas/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/products/', include('products.urls')),
     path('api/stock/', include('stock.urls')),
