025a302db74286fc809412c27cc5528689bc79c1
hani7 <h.informatique7@gmail.com>
2026-05-18 00:01:09 +0100

    Update cpanel paths to use baitmtzi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,18 @@
 import sys
 import os
 
-# 1. Ajouter le dossier de votre projet au PATH de Python
-sys.path.insert(0, os.path.dirname(__file__))
+# 1. Add the project directory to the Python path
+sys.path.insert(0, '/home/baitmtzi/forcli')
 
-# 2. Indiquer à Django d'utiliser les settings de production
+# 2. Add the virtualenv site-packages (cPanel Python App path)
+venv_path = '/home/baitmtzi/forcli'
+activate_this = os.path.join(venv_path, 'bin', 'activate_this.py')
+if os.path.exists(activate_this):
+    exec(open(activate_this).read(), {'__file__': activate_this})
+
+# 3. Tell Django to use production settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'for.settings_production')
 
-# 3. Lancer l'application Django
+# 4. Launch the Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
