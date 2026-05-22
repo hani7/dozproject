e6d7ff61db61f8ab788ecfb78fdff4cf3fcb4edf
hani7 <h.informatique7@gmail.com>
2026-05-22 21:06:16 +0100

    optimise

--- test_script.py
+++ test_script.py
@@ -12,7 +12,15 @@ try:
     client.force_authenticate(user=user)
     response = client.post('/api/auth/users/update_location/', {'latitude': 36.7, 'longitude': 3.1}, format='json')
     print('CODE:', response.status_code)
-    print('BODY:', response.content)
+    if response.status_code == 500:
+        import re
+        content = response.content.decode('utf-8')
+        m1 = re.search(r'<pre class="exception_value">(.*?)</pre>', content, re.IGNORECASE | re.DOTALL)
+        if m1:
+            print('EXCEPTION:', m1.group(1))
+        m2 = re.search(r'<textarea id="traceback_area".*?>(.*?)</textarea>', content, re.IGNORECASE | re.DOTALL)
+        if m2:
+            print('TRACEBACK:', m2.group(1))
 except Exception as e:
     import traceback
     traceback.print_exc()
